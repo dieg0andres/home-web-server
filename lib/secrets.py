55e70f62376673471742623db3bdab74d9f41f1f
Diego Galindo <diego.a.galindo@gmail.com>
2022-10-15 00:41:18 -0500

    adding github password to secrets.py

--- lib/secrets.py
+++ lib/secrets.py
@@ -6,3 +6,4 @@ PSSWD = 'xpnverolaoitewvg'
 HOME_SERVER = 'http://192.168.86.32:8080'
 GARAGE_DOOR_CONTROLLER = 'http://192.168.86.36:8080'
 
+# github password: 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
